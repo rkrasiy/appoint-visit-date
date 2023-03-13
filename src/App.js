b20423841d319be3fd80fc6562e7cda05d8be1c8
rkrasiy <krasiyruslan@gmail.com>
2023-03-13 17:57:04 +0100

    store

--- src/App.js
+++ src/App.js
@@ -1,42 +1,57 @@
 import React from 'react';
 import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
+    ChakraProvider,
+    Box,
+    Grid,
+    theme,
+    FormControl,
+    FormLabel,
+    Input,
+    Select,
+    FormErrorMessage,
+    FormHelperText,
 } from '@chakra-ui/react';
+import { useState } from 'react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { Logo } from './Logo';
 
 function App() {
-  return (
-    <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
-    </ChakraProvider>
-  );
+    const [input, setInput] = useState('');
+
+    const handleInputChange = e => setInput(e.target.value);
+
+    const isError = input === '';
+
+    return (
+        <ChakraProvider theme={theme}>
+        <Box textAlign="center" fontSize="xl">
+            <Grid minH="100vh" p={3}>
+            <FormControl isRequired>
+                <FormLabel>First name</FormLabel>
+                <Input placeholder="First name" />
+            </FormControl>
+            <FormControl isRequired>
+                <FormLabel>Email</FormLabel>
+                <Input type="email" value={input} onChange={handleInputChange} />
+                {!isError ? (
+                <FormHelperText>
+                    Enter the email you'd like to receive the newsletter on.
+                </FormHelperText>
+                ) : (
+                <FormErrorMessage>Email is required.</FormErrorMessage>
+                )}
+            </FormControl>
+            <FormControl isRequired>
+                <FormLabel>Country</FormLabel>
+                <Select placeholder="Select country">
+                <option>United Arab Emirates</option>
+                <option>Nigeria</option>
+                </Select>
+            </FormControl>
+            </Grid>
+        </Box>
+        </ChakraProvider>
+    );
 }
 
 export default App;
---
+++ src/store.js
@@ -0,0 +1,10 @@
+const owner = {
+    "id": 1,
+    "first_name": "Timmi",
+    "last_name": "Andrews",
+    "email": "tandrews0@ning.com",
+    "brand_name": "Westlake Chemical Corporation",
+    "brand_img": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABGdBTUEAAK/INwWK6QAAABl0RVh0U29mdHdhcmUAQWRvYmUgSW1hZ2VSZWFkeXHJZTwAAAJfSURBVDjLpZNNbxJRFIb7A/wF/A5YunTDrpouujNxY8LGxEVTVyU11UVjCmEsUUyb1gYqEWuqtqmRWukUimksH6UMHwIW6FCYwWFgYBjKcc6FGam68ybvZuY87/m4544BwNiobiyCQZVJlVnV5FDm4TfDn/Gj4DVVxgdvBIvv4IwKHafp2MkpF40nuP2jJP1qL0dNeXkLxmDsFYMhfN0TKFujp1mGrQkgSl1QLvtEjZYMpQoPwaM4s7STtWKsZqIZGBGOJ7+L7Y4CeCS5B7zYBU5Vs9Mj30RJhv1wRHRtpdDESAywLywbM2twVZCh8lOGt+EKsHUZyvUOlPiObrKzG2TurbHYjgENTD76B4Vlj8II3noYgI3DCoHPam0iPMncOTi8IQpZNDAHv6Vo7BlLRVDLenN2j+h1iCVwodoGoaXARV2C5fV3NLJoMBmJnXA4rFqjS2DMWOTaKvyZaOJRCPwxDnIViRjJyiWsudc5ZInBcTRODLB8DcZAAs8dwPiMn/zLstKwii4sr7zUDcxfviboutiBhqTovWLgxBx9Bc6ct8jNpIt1cLjcegsmtz9DFUo16PeBgPkLiZQ7PvOJwAimyy1IlVrQ7fVh9zABVucHfYiG+56qxR8IM5wwmDJmQyGsgclSkyTIqNntz1aZO8704Bq1RXJsRK2bHwMiyw8C601FrwaXCTOnizzYXB5x2rH1e5FGV3neHbauejeZUCQDBVYgM8GeE3kOtgNRmHcsMVP293+v8uhjuvsib5l9vk09WVyhHU+d3IKd4h7bXPS0zUfdppL/fkz/85x/AR14FVfMwp4lAAAAAElFTkSuQmCC",
+    "greatings": 0.6635803519,
+    "date_period": "10/18/2022"
+  }
